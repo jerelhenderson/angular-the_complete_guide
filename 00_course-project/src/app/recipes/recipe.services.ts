050b57e425668d32a72dae959aa1eb3427f7b112
Jerel Henderson <henderson.jerel@yahoo.com>
2021-11-13 23:38:31 -0600

    Add ability to send data from recipes -> shopping-list via service

--- 00_course-project/src/app/recipes/recipe.services.ts
+++ 00_course-project/src/app/recipes/recipe.services.ts
@@ -1,11 +1,20 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared-features/ingredient.model";
 import { Recipe } from "./recipe.model";
+
+@Injectable()
 
 export class RecipeService {
     selectedRecipe = new EventEmitter<Recipe>();
-    
+
     private recipes: Recipe[] = [
-        new Recipe('Test Recipe', 'This is merely a test', 'https://stat.ameba.jp/user_images/20190720/06/hiro3ch/cd/e4/j/o1080080914507251056.jpg')
+        new Recipe('天ぷら',
+        '美味しい天ぷら',
+        'https://stat.ameba.jp/user_images/20190720/06/hiro3ch/cd/e4/j/o1080080914507251056.jpg',
+        [
+            new Ingredient('えび', 4),
+            new Ingredient('サツマイモ', 1)
+        ])
     ];
 
     getRecipes() {
