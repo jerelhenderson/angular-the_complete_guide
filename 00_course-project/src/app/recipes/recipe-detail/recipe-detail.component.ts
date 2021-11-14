c699f2494d024abc4c758547ae23d55ef838cce8
Jerel Henderson <henderson.jerel@yahoo.com>
2021-11-14 00:01:12 -0600

    Commit files missing from previous (send data b/t components via service)

--- 00_course-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ 00_course-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.services';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -10,9 +11,12 @@ import { Recipe } from '../recipe.model';
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
   }
 
+  addIngredientsToList() {
+    this.recipeService.pushIngredientToList(this.recipe.ingredients);
+  }
 }
--- 00_course-project/src/app/recipes/recipe.services.ts
+++ 00_course-project/src/app/recipes/recipe.services.ts
@@ -1,9 +1,10 @@
 import { EventEmitter, Injectable } from "@angular/core";
+
 import { Ingredient } from "../shared-features/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.services";
 import { Recipe } from "./recipe.model";
 
 @Injectable()
-
 export class RecipeService {
     selectedRecipe = new EventEmitter<Recipe>();
 
@@ -17,7 +18,13 @@ export class RecipeService {
         ])
     ];
 
+    constructor(private shoppingListService: ShoppingListService) {};
+
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    pushIngredientToList(ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
     }
 }
